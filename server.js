99a68017f09656413160a855e2f43862d9f98a49
Cristiano Solarino <cristiano.solarino@brightminded.com>
2014-03-23 12:32:36 +0000

    using env variable for server port

--- server.js
+++ server.js
@@ -27,6 +27,6 @@ server.del(clog, api.unclog.bind(api));
 /* Routes end here 
 --------------------- */
 
-server.listen(8080, function(){
+server.listen(process.env.PORT || 8080, function(){
   console.log('%s listening at %s', server.name, server.url);
 });
